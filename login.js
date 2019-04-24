c2885527cabf2a6755871858f14bfa298e6a5e0e
Jasmine <singh486@purdue.edu>
2019-04-24 19:24:14 -0400

    SignUp & error handling

--- login.js
+++ login.js
@@ -5,5 +5,25 @@ function validate(){
 }
 
 function signUp(){
-    console.log("signUp")
+    var email = document.getElementById("email").value
+    var password = document.getElementById("pwd").value
+    ValidateEmail(email)
+    ValidatePassword(password)
+    
+    console.log(email)
+    console.log(password)
+}
+
+function ValidateEmail(mail){
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)){
+    return (true)
+  }
+  alert("You have entered an invalid email address!")
+  return (false)
+}
+
+function ValidatePassword(pass){
+    if(pass.length == 0){
+        alert("Please enter a password")
+    }
 }
