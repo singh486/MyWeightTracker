ad235e2c4a1ee671d2bace97e93eccb020340e11
Neha <@purdue.edu>
2019-04-24 19:31:47 -0400

    User authentication and database collections

--- login.js
+++ login.js
@@ -5,10 +5,14 @@ function validate(){
     //validate user information here
     console.log("test")
     window.location.href = "mainPage.html"
+}
 
-    var user = firebase.auth().currentUser;
-    var email = firebase.auth().email;
-    var password = firebase.auth().password;
+function signUp(){
+    var email = document.getElementById("email").value
+    var password = document.getElementById("pwd").value
+    ValidateEmail(email)
+    ValidatePassword(password)
+    
     let db = firebase.firestore();
     db.collection('Users').doc(email).set({
         email: email,
@@ -16,6 +20,23 @@ function validate(){
     }).then(() => {
         console.log("Successfully added user to firebase");
     }).catch({
-        
+
     })
+    console.log(email)
+    console.log(password)
 }
+
+
+function ValidateEmail(mail){
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)){
+      return (true)
+    }
+    alert("You have entered an invalid email address!")
+    return (false)
+  }
+  
+  function ValidatePassword(pass){
+      if(pass.length == 0){
+          alert("Please enter a password")
+      }
+  }
